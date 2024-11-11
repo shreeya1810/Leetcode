126b6d8c428d498d6fe4187c7aaa7f258347602e
Shreeya <108709383+shreeya1810@users.noreply.github.com>
2024-11-12 01:36:26 +0530

    Time: 557 ms (37.58%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select today.id from Weather yesterday cross join weather today where datediff(today.recordDate, yesterday.recordDate) = 1 and today.temperature > yesterday.temperature
